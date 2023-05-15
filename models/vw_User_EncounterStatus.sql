4c1c6a98226beb801ad1f0d3253cc7940d9c498e
aravipati@landmarkhealth.org <aravipati@landmarkhealth.org>
2023-05-15 14:17:53 +0000

    commit

--- models/vw_User_EncounterStatus.sql
+++ models/vw_User_EncounterStatus.sql
@@ -6,9 +6,10 @@ with final as (
         Provider_login,
         Provider_name,
         Encounter_Doc_Status,
+        Encounter_date,
         Count(1) as visits
     from  {{ ref('vw_User_EncounterDetails') }}
-    Group by 1,2,3,4
+    Group by 1,2,3,4,5
 )
 
 select * from final
